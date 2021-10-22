6e6851f1bda14cfe153bb4e86e59617d878ed69b
Caleb Carlson <caleb.carlson@hpe.com>
2021-10-22 08:26:33 -0600

    Attempt None comparison

--- main.py
+++ main.py
@@ -18,6 +18,19 @@ def exists_in_collection(field, gis_join, collection):
     return collection.find_one({field: {"$regex": f".*{gis_join}.*"}}) is not None
 
 
+def get_gis_join_field(collection):
+    first_record = collection.find_one()
+    if "GISJOIN" in first_record.keys():
+        return "GISJOIN"
+    elif "gis_join" in first_record.keys():
+        return "gis_join"
+    elif "properties" in first_record.keys():
+        if "GISJOIN" in first_record["properties"].keys():
+            return "properties.GISJOIN"
+    else:
+        return None
+
+
 def main():
     client = MongoClient("mongodb://lattice-100:27018")
     database_name = "sustaindb"
@@ -30,32 +43,33 @@ def main():
     for collection_name in sorted(collection_names):
         print(f"Evaluating collection {collection_name}...")
         collection = db[collection_name]
-        first_record = collection.find_one()
+        found = False
 
         for gis_join in gis_joins:
-            collections_supported_by_gis_join[gis_join] = []
+            # Add empty list if no entry exists yet
+            if gis_join not in collections_supported_by_gis_join:
+                collections_supported_by_gis_join[gis_join] = []
 
-            found = False
-
-            if "GISJOIN" in first_record.keys():
-                if exists_in_collection("GISJOIN", gis_join, collection):
+            found_in_field = get_gis_join_field
+            if found_in_field:
+                if exists_in_collection(found_in_field, gis_join, collection):
                     collections_supported_by_gis_join[gis_join].append(collection_name)
                     found = True
-
-            elif "gis_join" in first_record.keys():
-                if exists_in_collection("gis_join", gis_join, collection):
-                    found = True
-                    collections_supported_by_gis_join[gis_join].append(collection_name)
-
-            elif "properties" in first_record.keys():
-                if "GISJOIN" in first_record["properties"].keys():
-                    if exists_in_collection("properties.GISJOIN", gis_join, collection):
-                        found = True
-                        collections_supported_by_gis_join[gis_join].append(collection_name)
 
             print("Found:", found)
 
     print(collections_supported_by_gis_join)
+
+    metadata_collection_name = "state_gis_join_metadata"
+    metadata_collection = db[metadata_collection_name]
+    index = 0
+    for gis_join in sorted(gis_joins):
+        collections_found_in = collections_supported_by_gis_join[gis_join]
+        metadata_collection.insert_one({
+            "_id": index,
+            "gis_join": gis_join,
+            "collections_supported": collections_found_in
+        })
 
 
 if __name__ == '__main__':
