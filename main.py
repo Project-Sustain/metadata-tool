74ed08646dbec412e0e17ee5f9451744ddbfbe58
Caleb Carlson <caleb.carlson@hpe.com>
2021-10-21 20:11:03 -0600

    List collection names in mongo

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-
-def print_hi(name):
-
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+from pymongo import MongoClient
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    client = MongoClient("mongodb://lattice-100:27018")
+    database_name = "sustaindb"
+    db = client[database_name]
+    collection_names = db.list_collection_names()
+    for collection_name in collection_names:
+        print(f"Collection: {collection_name}")
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
